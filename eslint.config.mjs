010a8ec6c48f9f4c9749379b61dc20ac827e98bc
Arpit Doshi <arpit.k.doshi@gmail.com>
2024-12-23 21:46:21 +0530

    feat: basic setup and configuration before test setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,63 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from '@eslint/compat';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...fixupConfigRules(
+    compat.extends(
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:unicorn/recommended',
+      'plugin:import/recommended',
+      'plugin:playwright/recommended',
+      'plugin:prettier/recommended',
+    ),
+  ),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-array-reduce': 'off',
+
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            e2e: true,
+          },
+          replacements: {
+            props: false,
+            ref: false,
+            params: false,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.js'],
+
+    rules: {
+      'unicorn/prefer-module': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
